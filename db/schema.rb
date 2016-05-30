4fe3eda1d52bcc4c71c8f49bf5cd61df7e2b8b3c
Eric Stephens <perkinsdrive@gmail.com>
2016-05-30 06:57:38 +0000

    Assignment 28: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529081258) do
+ActiveRecord::Schema.define(version: 20160530045939) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "answers", ["post_id"], name: "index_answers_on_post_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -32,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160529081258) do
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
