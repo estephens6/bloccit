3ad60bd720dd94abed5a7d8d92a991db4c653a10
Eric Stephens <perkinsdrive@gmail.com>
2016-05-31 04:42:21 +0000

    Assignment 30: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,21 @@
 require 'random_data'
 
 #Create Posts
+
+puts "#{Post.count}"
+
+#code with error I wrote / created two posts with the same title of "Cire"
+#Post.find_or_create_by(title: "Cire") do
+#   Post.create!(
+#      title: "Cire",
+#     body: "What's the first rule? You don't talk about X, what's the second rule? YOU DO NOT TALK ABOUT X."
+#    )
+#end
+
+#Alt code
+Post.find_or_create_by(title: "A unique title", body: "Body text")
+
+
 50.times do
 
     Post.create!(
@@ -19,6 +34,9 @@ posts = Post.all
         body: RandomData.random_paragraph
     )
 end
+
+
+puts "#{Post.count}"
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
