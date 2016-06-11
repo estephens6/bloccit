722b9a9738886783e7205de8bdb7225d4b9dcf59
Eric Stephens <perkinsdrive@gmail.com>
2016-06-11 10:26:02 +0000

    Assignment 33: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'random_data'
+include RandomData
 
 #Create Posts for data base here
 50.times do
@@ -20,6 +20,17 @@ posts = Post.all
     )
 end
 
+# Create Questions
+100.times do
+    Question.create!(
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph,
+        resolved: false
+    )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
+ 
