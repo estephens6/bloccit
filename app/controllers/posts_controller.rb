b3c996e1de51a225f9a058dfb53d9ec4a9b33c07
Eric Stephens <perkinsdrive@gmail.com>
2016-06-18 07:25:26 +0000

    Checkpoint 34: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all    
-  end
+#  def index
+#    @posts = Post.all    
+#  end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
   
@@ -15,10 +16,16 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+    
+    @post.topic = @topic
     
     if @post.save
       flash[:notice] = "Post was saved sucessfully."
-      redirect_to @post
+
+#      redirect_to @post
+      redirect_to [@topic, @post]
+
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
@@ -36,10 +43,11 @@ class PostsController < ApplicationController
     
     if @post.save
       flash[:notice] = "Post was updated successfully."
-      redirect_to @post
+#      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
-      render :editend
+      render :edit
     end
   end
   
@@ -48,7 +56,8 @@ class PostsController < ApplicationController
     
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+#      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post."
       render :show
