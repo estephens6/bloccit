95e1535bbd1f0a89ae130b3c23e6741c41ce2fc5
Eric Stephens <perkinsdrive@gmail.com>
2016-06-21 09:04:08 +0000

    6/20b

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+#  get 'sponsored_posts/index'
+
+  get 'sponsored_posts/show'
+
+  get 'sponsored_posts/new'
+
+  get 'sponsored_posts/edit'
+
 #    resources :topics
 #    resources :posts
     resources :topics do
