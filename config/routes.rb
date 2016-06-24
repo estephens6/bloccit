ada94db737a5479ac76f8d4ea28f94a8c3c7c9b5
Eric Stephens <perkinsdrive@gmail.com>
2016-06-24 04:24:09 +0000

    assignment 34 bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 #  get 'sponsored_posts/index'
 
-  get 'sponsored_posts/show'
+#  get 'sponsored_posts/show'
 
-  get 'sponsored_posts/new'
+#  get 'sponsored_posts/new'
 
-  get 'sponsored_posts/edit'
+#  get 'sponsored_posts/edit'
 
 #    resources :topics
 #    resources :posts
