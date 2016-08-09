8f1324fac5689ff3e436dd351a816ceeefd2748d
Eric Stephens <perkinsdrive@gmail.com>
2016-08-09 05:27:38 +0000

    Checkpoint 46: completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,9 @@
 class Topic < ActiveRecord::Base
     has_many :posts, dependent: :destroy
     
-   has_many :labelings, as: :labelable
+    has_many :labelings, as: :labelable
  # #13
-   has_many :labels, through: :labelings
+    has_many :labels, through: :labelings
+
+    scope :visible_to, -> user { user ? all : where(public: true) }
 end
